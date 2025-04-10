71d166dbd2b40f967820ac7ce73b0eeda917689f
Ombrali <menoudamelie@gmail.com>
2025-04-10 19:15:55 +0200

    ff

--- OurMain.py
+++ OurMain.py
@@ -45,7 +45,6 @@ if __name__ == '__main__':
  
     F = Monitoring_Fleet(Map, buffer_size, state_shape, pretrained_folder, True, num_stacked, alpha)
     F.add_drones(drone_init_pos) 
-    print(F.list_of_drones[0].list_of_move_actions)
 
     #----- Create save folder paths -----
     
