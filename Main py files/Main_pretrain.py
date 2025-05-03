bbcd6219e43ebd5048e06aa8bd4d7acbbbbeccc4
Ombrali <menoudamelie@gmail.com>
2025-05-03 16:13:24 +0200

    tidy up

---
+++ Main py files/Main_pretrain.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+from RegionMap import RegionMap
+from Fleet import Monitoring_Fleet
+
+import numpy as np
+import os 
+from datetime import datetime
+
+if __name__ == '__main__':
+
+    N_episodes = 1000
+    ep_len     = 5
+    
+    buffer_size = 6000
+    batch_size = 32
+    
+    # ----- Create a map -----
+    
+    x_size = 20
+    y_size = 30
+
+    list_of_small_pertb = [[0,4], [13,5], [17,25]]
+    list_of_big_pert = [[6,9],[17,25]]
+    
+    Map = RegionMap(x_size, y_size, list_of_small_pertb, list_of_big_pert)
+    Map.initialize_importance_map()
+    
+    # ----- Create a fleet -----
+    
+    drone_init_pos = [[5,5], [5,20], [18, 4], [19,21]]
+      
+    state_shape    = [ep_len, 13]    
+    alpha = 1e-4
+    num_stacked = 2
+    
+    pretrained_folder = None    
+ 
+    F = Monitoring_Fleet(Map, buffer_size, state_shape, pretrained_folder, True, num_stacked, alpha)
+    F.add_drones(drone_init_pos)    
+    
+    #----- Create save folder paths -----
+    
+    np.set_printoptions(threshold=np.inf)
+    
+    current_folder = os.getcwd()
+    save_folder = current_folder + "/Results"
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)    
+        
+    now = datetime.now()
+    date_time = now.strftime("%m_%d_%Y_%H_%M_%S")    
+    
+    current_results = save_folder + "/" + date_time + "_pretraining"
+    if not os.path.exists(current_results):
+        os.makedirs(current_results)    
+    
+    #----- Simultaion pretraining -----
+    
+    load_data_link = '/Results/11_20_2024_02_07_05'
+    
+    #F.pretrain(current_results, N_iter=100000, load_data=load_data_link, plot_pretrained=True)
+    
+    #----- Simulation Continue training -----
+    
+    F.continue_training(current_results, lr=1e-4, n_lstm=2, N_iter=20000, load_data=load_data_link, pretrained_folder="/Results/11_20_2024_02_14_49_pretraining/Saved_models/policy_network_49999.pt", plot_pretrained=True)    
+     
